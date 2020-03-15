7ee583a99f15222483786f12bb79899b07204070
kkamikoon <rootsik1221@gmail.ciocom>
2020-03-16 02:57:56 +0900

    ~view-project

---
+++ study019_single_file_component_your_component.vue
@@ -0,0 +1,21 @@
+<!-- HTML Tag -->
+<template>
+    <div>
+        <span>
+            <button>{{ message }}</button>
+        </span>
+    </div>
+
+</template>
+
+<!-- Javascript -->
+<script>
+export default{
+    
+}
+</script>
+
+<!-- CSS Style -->
+<style>
+    
+</style>
